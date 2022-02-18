58ba8aed6dbaa81c2a290e419c2eafb136fafd1e
hebrewsnabla <srwang20@fudan.edu.cn>
2022-02-18 18:49:39 +0800

    ci grad and quasi-newton ci opt

--- nof.py
+++ nof.py
@@ -1,6 +1,8 @@
 from pyscf import lib, ao2mo, scf
 import numpy as np
 from automr.autocas import check_uno
+from automr.dump_mat import dump_mo
+from qnewton import qn_iter
 
 einsum = lib.einsum
 
@@ -27,14 +29,23 @@ class PNOF():
         #self.nopen = 
         nact, ncore, nex = check_uno(self.mo_occ*2, 1.99999)
         print('ncore %d nact %d nex %d' % (ncore, nact, nex))
+        #exit()
         self.ncore = ncore
         self.npair = (nact - self.nopen)//2
+        print('occ', self.mo_occ[:ncore+nact])
         Delta, Pi = get_DP(self.mo_occ, self.ncore, self.npair, self.nopen)
         h_mo = h1e(self._hf, mo)
         J,K = self.ao2mo()
         e0 = energy_elec(self.mo_occ, h_mo, J, K, Delta, Pi)
         e0 += self._hf.energy_nuc()
         print('PNOF5(GVB) energy %.6f' % e0)
+        
+        ao_ovlp = self.mol.intor_symmetric('int1e_ovlp')
+        guess = np.ones(self.npair)*0.9
+        guess = np.arange(0.99, 0.5, -0.48/(self.npair-1))
+        new_occ = get_occ(self, mo, ao_ovlp, self.ncore, self.npair, self.nopen, guess,
+                          h_mo, J, K)
+        print('intrinsic occ', new_occ[:ncore+nact])
 
         get_grad(self, self.mo_occ, mo, h_mo, self.ncore, self.npair, self.nopen, Delta, Pi)
         
@@ -51,9 +62,10 @@ class PNOF():
         _eri = self.mol.intor('int2e')
         #else:
         #    _eri = self._hf._eri
-        print(_eri.shape, mo.shape)
-        J = einsum('ijkl,ip,jp,kq,lq -> pq', _eri, mo, mo, mo, mo)
-        K = einsum('ijkl,ip,jq,kp,lq -> pq', _eri, mo, mo, mo, mo)
+        #print(_eri.shape, mo.shape)
+        #J = einsum('ijkl,ip,jp,kq,lq -> pq', _eri, mo, mo, mo, mo)
+        #K = einsum('ijkl,ip,jq,kp,lq -> pq', _eri, mo, mo, mo, mo)
+        J, K = _ao2mo(mo, mo, _eri)
         return J, K
 
 def energy_elec(mo_occ, h_mo, J, K, Delta, Pi):
@@ -85,6 +97,87 @@ def get_DP(f, ncore, npair, nopen):
     #print(Pi[:15,:15])
     return Delta, Pi
 
+def get_occ(nof, mo, ao_ovlp, ncore, npair, nopen, guess, h_mo, J, K):
+    new_occ = np.zeros(mo.shape[-1])
+    new_occ[:ncore] = 1.0
+    #guess = np.ones(npair)*0.99
+    #guess = np.array([0.995, 0.995, 0.981, 0.981, 0.713])**0.5
+    guess = guess**0.5
+    print('guess ci', guess)
+    #guess = np.hstack((np.ones(ncore), guess))
+    mo_act = mo[:, :ncore+2*npair+nopen]
+    def update_occ(X, ncore, npair, nopen, nmo):
+        occ = np.zeros(nmo)
+        occ[:ncore] = 1.0
+        occ[ncore:ncore+npair] = X**2
+        occ[ncore+npair:ncore+2*npair] = np.flip(1.0 - X**2)
+        return occ
+    def get_grad(t):
+        X = np.sin(t)
+        return get_ci_grad(nof, X, mo_act, npair, ncore)*np.cos(t)
+    def get_E(t):
+        X = np.sin(t)
+        occ = update_occ(X, ncore, npair, nopen, len(new_occ))
+        Delta, Pi = get_DP(occ, ncore, npair, nopen)
+        return energy_elec(occ, h_mo, J, K, Delta, Pi)
+
+    new_t = qn_iter(np.arcsin(guess), get_grad, get_E)
+    ci = np.sin(new_t)
+    print('ci', ci)
+    new_occ = update_occ(ci, ncore, npair, nopen, len(new_occ))
+    
+    return new_occ
+
+def get_ci_grad(nof, guess, mo, npair, ncore):
+    #print('ci', guess)
+    mo_g = mo[:, ncore:ncore+npair]
+    mo_u = np.flip(mo[:, ncore+npair:], axis=1) 
+    mo_core = mo[:,:ncore]
+    #dump_mo(nof.mol, mo_g)
+    #dump_mo(nof.mol, mo_u)
+    _eri = nof.mol.intor('int2e')
+    Jgg, Kgg = _ao2mo(mo_g, mo_g, _eri)
+    Juu, Kuu = _ao2mo(mo_u, mo_u, _eri)
+    Jgu, Kgu = _ao2mo(mo_g, mo_u, _eri)
+    Jgc, Kgc = _ao2mo(mo_g, mo_core, _eri)
+    Juc, Kuc = _ao2mo(mo_u, mo_core, _eri)
+    lam0 = h1e(nof._hf, mo_g).diagonal() - h1e(nof._hf, mo_u).diagonal() + einsum('ki->k', 2*Jgc - Kgc - 2*Juc + Kuc)
+    lam0 += (Jgg.diagonal() - Juu.diagonal())*0.5
+    def tri_k1(m):
+        #return np.triu(m, k=1) + np.tril(m,k=-1)
+        return m - np.diag(m.diagonal())
+    uJgg, uKgg, uJuu, uKuu, uJgu, uKgu = map(tri_k1, (Jgg, Kgg, Juu, Kuu, Jgu, Kgu))
+    #print(Jgg, '\n', Kgg, '\n', Juu, '\n', Kuu,  '\n',Jgu
+    #, '\n', Kgu
+    #)
+    lam1 = einsum('i,ik->k', guess**2, (2*uJgg - uKgg -2*uJgu + uKgu)) 
+    lam2 = einsum('i,ik->k', 1.0 - guess**2, (2*uJgu.T - uKgu.T -2*uJuu + uKuu))
+    lam = lam0 + lam1 + lam2
+    #print(1.0 - guess**2)
+    #print((2*uJgu.T - uKgu.T -2*uJuu + uKuu))
+    #print(lam0, lam1, lam2)
+    lam *= 2.0
+    tmp1 = 2*einsum('k,k->k', guess, lam) 
+    tmp2 = 2*einsum('k,k,k->k', 1 - 2*guess**2, -1.0/np.sqrt(1.0 - guess**2), Kgu.diagonal())
+    #print(1 - 2*guess**2, 1.0/np.sqrt(1.0 - guess**2), Kgu.diagonal())
+    ci_grad = tmp1 + tmp2
+    #print(tmp1, tmp2)
+    #print('ci_grad', ci_grad)
+    hdiag = 2*lam + 2*einsum('k,k,k,k->k', guess, 1.0/(1.0 - guess**2)**1.5, 1.0 + 2*(1.0 - guess**2), Kgu.diagonal())
+    #print(lam)
+    #print(hdiag)
+    return ci_grad, hdiag
+
+    
+
+def _ao2mo(mo1, mo2, ao_eri):
+    J = einsum('ijkl,ip,jp,kq,lq -> pq', ao_eri, mo1, mo1, mo2, mo2)
+    K = einsum('ijkl,ip,jq,kp,lq -> pq', ao_eri, mo1, mo2, mo1, mo2)
+    return J, K
+
+def rotate_ci(ci, ):
+    pass
+
 def get_grad(nof, f, mo, h_mo, ncore, npair, nopen, Delta, Pi):
     occ = ncore + 2*npair + nopen
     mo_o = mo[:,:occ]
@@ -103,7 +196,8 @@ def get_grad(nof, f, mo, h_mo, ncore, npair, nopen, Delta, Pi):
     eri2 = ao2mo.general(_eri, (mo_o, mo_v, mo_o, mo_o), max_memory=nof.max_memory)
     goo = einsum('qp,pq->pq', df, h_mo[:occ,:occ]) + einsum('qpk,pqkk->pq', da, eri1) + einsum('qpk,pkqk->pq', db, eri1)
     gov = einsum('q,pq->pq', f[:occ], h_mo[occ:,:occ]) +  einsum('qk,qpkk->pq', a, eri2) + einsum('qk,kpkq->pq', b, eri2)
-    print(goo)
-    print(gov)
+    #print(goo)
+    #print(gov)
 
 def gen_g_hop(mf, mo, mo_occ):
+    pass
---
+++ qnewton.py
@@ -0,0 +1,136 @@
+import numpy as np
+import copy
+import sys
+
+a = 3.00
+b = 2.00
+k = 1.000
+k1 = 0.100
+k2 = 0.03 #float(sys.argv[1])
+#A = 1
+#B = 0.5
+#v = 2
+
+
+
+'''def update(X,E,h,Xs):
+    print("update %f,%f -> %f,%f"%(X[h][0],X[h][1],Xs[0],Xs[1]))
+    X[h] = Xs
+    newX = X
+    E[h] = getE(Xs)
+    newE = E
+    return newX, newE
+
+def findh(E):
+    Emax = -100.0
+    h = -1
+    for i in range(len(E)):
+        if E[i] > Emax:
+            h = i
+            Emax = E[i]
+    return h, Emax
+def conv(dx):
+    thresh = 0.002
+    cv = abs(dx[0]) < thresh and abs(dx[1]) < thresh
+    return cv
+'''
+def constrain(X, Xmin, Xmax):
+    #print(X)
+    over = np.heaviside(X - Xmax, 0.0)
+    under = np.heaviside(X - Xmin, 0.0)
+    X = X*(1.0-over) + Xmax*over
+    X = X*under + Xmin*(1.0-under)
+    #print(X)
+    return X
+
+def qn_iter(X, getf, getE, maxstep=0.05, maxiter=30):
+    #Xmin, Xmax = Xrange
+    E = getE(X)
+    f, hdiag = getf(X)
+    print('cycle    maxq     dE    maxg')
+    print("   0                   %.4f"% abs(f).max())
+    nx = len(X)
+    oldE = E
+    oldX = copy.copy(X)
+    oldf = copy.copy(f)
+    oldalpha = 0.5
+    #oldG = np.identity(nx)
+    oldG = np.diag(hdiag**(-1))
+    while(True):
+        q = -oldalpha*oldf
+        q = constrain(q, -maxstep, maxstep)
+        X = oldX + q
+        #X = constrain(X, Xmin, Xmax)
+        E = getE(X)
+        f, hdiag = getf(X)
+        dE = E - oldE
+        if E > oldE:
+            print("reset alpha")
+            oldalpha /= 2
+        else:
+            #print("  1   %.3f %.3f %.3f "%(abs(q).max(), dE, abs(f).max()))
+            dump_cyc(1, q, dE, f)
+            print("Start Forming U")
+            U = -1*np.dot(oldG, f + oldf*(oldalpha-1))
+            d = f - oldf
+            #print("U: ", U, "d:", d)
+            aa = 1.0/np.dot(U.T, d)
+            T = np.dot(U.T, U) 
+            #print("a: %f  T: %f"%(aa,T), "aUf: %f"%((1/aa)*np.dot(U,oldf)))
+            if (1/aa < 1e-5*T) or (abs((1/aa)*np.dot(U.T,oldf)) > 1e-5):
+            #if False:
+                print("reset G")
+                #G = np.identity(nx)
+                G = np.diag(hdiag**(-1))
+                alpha = 0.5
+            else:
+                G = oldG + aa*np.einsum('i,j->ij', U, U)
+                alpha = 1
+            break
+    cyc = 2
+    while(True):
+        oldX = copy.copy(X)
+        oldE = E
+        oldalpha = alpha
+        oldG = copy.copy(G)
+        oldf = copy.copy(f)
+        #print(oldX, oldE)
+        q = -oldalpha*np.dot(oldG, oldf)
+        q = constrain(q, -maxstep, maxstep)
+        X = oldX + q
+        #X = constrain(X, Xmin, Xmax)
+        E = getE(X)
+        dE = E - oldE
+        f, hdiag = getf(X)
+        #print("Cycle %d  X: "%cyc, X, "E:", E, 'f:', f)
+        dump_cyc(cyc, q, dE, f)
+        U = -1*np.dot(oldG, f + oldf*(oldalpha-1))
+        d = f - oldf
+        #print(U, d)
+        aa = 1.0/np.dot(U.T, d)
+        T = np.dot(U.T, U) 
+        #print("a: %f  T: %.9f"%(aa,T), "aUf: %.9f"% ((1/aa)*np.dot(U,oldf)))
+        if (abs(aa*T > 1e5)) or (abs((1/aa)*np.dot(U.T,oldf)) > 1e-5):
+        #if False:
+            print("reset G")
+            #break
+            #G = np.identity(nx)
+            G = np.diag(hdiag**(-1))
+            alpha = 0.5
+        else:
+            G = oldG + aa*np.einsum('i,j->ij',U, U)
+            alpha = 1
+    
+        if np.dot(f.T, f) < 1e-10:
+            break
+        else:
+            cyc += 1
+        if cyc > maxiter:
+            print("conv not met")
+            break
+    return X
+
+def dump_cyc(cyc, q, dE, f):
+    print("  %2d    %.4f %.4f %.4f "%(cyc, abs(q).max(), dE, abs(f).max()))
+'''
+'''
